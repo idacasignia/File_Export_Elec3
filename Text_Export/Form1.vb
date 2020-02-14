38ef4845bd76b0aa0fa184617a677426fdcf3f2f
idacasignia <idacasignia@gmail.com>
2020-02-14 21:49:10 +0800

    third commit

--- Text_Export/Form1.vb
+++ Text_Export/Form1.vb
@@ -12,8 +12,45 @@
 
         My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & Label1.Text, True)
         My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & TextBox1.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & Label2.Text, True)
         My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & TextBox2.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & Label3.Text, True)
         My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.txt", "" & TextBox3.Text, True)
+
+        MsgBox("saved")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myFile As String, myFolder As String
+
+        myFolder = "C:\Users\idaca\Desktop"
+        myFile = myFolder & "Export.txt"
+
+
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & Label1.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & TextBox1.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & Label2.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & TextBox2.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & Label3.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.xml", "" & TextBox3.Text, True)
+
+        MsgBox("saved")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim myFile As String, myFolder As String
+
+        myFolder = "C:\Users\idaca\Desktop"
+        myFile = myFolder & "Export.txt"
+
+
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & Label1.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & TextBox1.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & Label2.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & TextBox2.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & Label3.Text, True)
+        My.Computer.FileSystem.WriteAllText("C:\Users\idaca\Desktop\Export.json", "" & TextBox3.Text, True)
 
         MsgBox("saved")
     End Sub
